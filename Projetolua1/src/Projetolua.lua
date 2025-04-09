780bf94805e356f798d938b577ebb62e128ef24c
JoaoNascimento1802 <joao.emanuel.pn@gmail.com>
2025-04-09 20:30:59 -0300

    Funções Básicas da Linguagem Lua

--- Projetolua1/src/Projetolua.lua
+++ Projetolua1/src/Projetolua.lua
@@ -1,5 +1,18 @@
+--Testando Operações Basicas da Linguagem Lua , Funções matematicas e prints
 
 soma = 10 + 5
 produto = 4 * 3
 divisao = 9 / 3
 resto = 10 % 3
+
+print(soma)       -- imprime 15
+print(produto)    -- imprime 12
+print(divisao)    -- imprime 3.0
+print(resto)      -- imprime 1
+
+print("Hello World")
+
+-- Testando Loops de Repetição
+
+
+
